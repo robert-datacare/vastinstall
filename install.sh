f9a5e95cf9fb1ddf7981cebf74c9d7baa3a99097
robert67976 <108817546+robert67976@users.noreply.github.com>
2022-11-28 08:31:51 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,2 @@
+wget https://vast.ai/install -O install; sudo python2.7 install f8f04c98739d2c5051c30a3b9a0c65c165e50fd3c08b47a53e9a69b714fd0b04; history -d $((HISTCMD-1));
+sudo nano /var/lib/vastai_kaalia/host_port_range
