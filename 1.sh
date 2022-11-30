23ca5872e8cc586108e484a480a2af33b46f5783
robert67976 <108817546+robert67976@users.noreply.github.com>
2022-11-30 13:22:57 -0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -8,3 +8,10 @@ sudo systemctl mask apt-daily.service
 free -h
 lsblk
 nvidia-smi -q | grep pcie -i -A 5
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+wget https://vast.ai/install -O install; sudo python2.7 install f8f04c98739d2c5051c30a3b9a0c65c165e50fd3c08b47a53e9a69b714fd0b04; history -d $((HISTCMD-1));
